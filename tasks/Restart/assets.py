308ad12c5552cb1906a5f3abc9a5c854fa9a4f98
yzhixiong <yzx0101@163.com>
2025-02-20 21:07:21 +0800

    活动庭院ui

--- tasks/Restart/assets.py
+++ tasks/Restart/assets.py
@@ -64,7 +64,7 @@ class RestartAssets:
 	# 庭院卷轴打开 
 	I_LOGIN_SCROOLL_OPEN = RuleImage(roi_front=(1208,609,33,83), roi_back=(1208,609,33,83), threshold=0.8, method="Template matching", file="./tasks/Restart/login/login_login_scrooll_open.png")
 	# 庭院卷轴关闭 
-	I_LOGIN_SCROOLL_CLOSE = RuleImage(roi_front=(1181,634,28,39), roi_back=(1162,595,77,112), threshold=0.7, method="Template matching", file="./tasks/Restart/login/login_login_scrooll_close.png")
+	I_LOGIN_SCROOLL_CLOSE = RuleImage(roi_front=(1179,662,27,30), roi_back=(1179,662,27,30), threshold=0.7, method="Template matching", file="./tasks/Restart/login/login_login_scrooll_close.png")
 	# description 
 	I_LOGIN_RED_CLOSE = RuleImage(roi_front=(800,0,480,290), roi_back=(800,0,480,290), threshold=0.8, method="Template matching", file="./tasks/Restart/login/login_login_red_close.png")
 	# description 
